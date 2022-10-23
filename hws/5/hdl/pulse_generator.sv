ccfa4061bb10d3086776ec48f0e8696422c5c57b
Devlin Ih <dih@olin.edu>
2022-10-23 16:08:03 -0400

    Implement pulse generator

--- hws/5/hdl/pulse_generator.sv
+++ hws/5/hdl/pulse_generator.sv
@@ -1,16 +1,29 @@
 /*
   Outputs a pulse generator with a period of "ticks".
-  out should go high for one cycle ever "ticks" clocks.
+  out should go high for one cycle every "ticks" clocks.
 */
 module pulse_generator(clk, rst, ena, ticks, out);
+   parameter N = 8;
 
-parameter N = 8;
-input wire clk, rst, ena;
-input wire [N-1:0] ticks;
-output logic out;
+   input wire clk, rst, ena;
+   input wire [N-1:0] ticks;
+   output logic       out;
 
-logic [N-1:0] counter;
-logic counter_comparator;
+   logic [N-1:0] counter;
+   logic [N-1:0] next_state;
 
+   // See schematic for details
+   always_comb begin
+      out = counter >= ticks;
+      next_state = ena ? (out ? 'b0 : counter+1)
+                       : counter;
+   end
 
+   always_ff @(posedge clk) begin
+      // counter <= rst ? N'b0 : next_state;
+
+      // Why does not specifying bus size make it work? I know linting
+      // complains but just curious as to why
+      counter <= rst ? 'b0 : next_state;
+   end
 endmodule
