67defa35d88c4f371f628ae9a30a60de0c0b3e36
Devlin Ih <dih@olin.edu>
2022-10-23 20:20:43 -0400

    Refactor slightly
    It's more to look at but it makes more sense

--- hws/5/hdl/pulse_generator.sv
+++ hws/5/hdl/pulse_generator.sv
@@ -14,16 +14,12 @@ module pulse_generator(clk, rst, ena, ticks, out);
 
    // See schematic for details
    always_comb begin
-      out = counter >= ticks;
-      next_state = ena ? (out ? 'b0 : counter+1)
+      out = ~|counter;
+      next_state = ena ? ((counter == ticks) ? 'b0 : counter + 1)
                        : counter;
    end
 
    always_ff @(posedge clk) begin
-      // counter <= rst ? N'b0 : next_state;
-
-      // Why does not specifying bus size make it work? I know linting
-      // complains but just curious as to why
       counter <= rst ? 'b0 : next_state;
    end
 endmodule
