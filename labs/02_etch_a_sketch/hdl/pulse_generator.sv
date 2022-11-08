8542a6416bf305693eb8027e04ec8e1cb6984666
Devlin Ih <dih@olin.edu>
2022-11-07 20:31:44 -0500

    add hw5 solutions again

--- labs/02_etch_a_sketch/hdl/pulse_generator.sv
+++ labs/02_etch_a_sketch/hdl/pulse_generator.sv
@@ -1,16 +1,25 @@
 /*
   Outputs a pulse generator with a period of "ticks".
-  out should go high for one cycle ever "ticks" clocks.
+  out should go high for one cycle every "ticks" clocks.
 */
 module pulse_generator(clk, rst, ena, ticks, out);
+   parameter N = 8;
 
-parameter N = 8;
-input wire clk, rst, ena;
-input wire [N-1:0] ticks;
-output logic out;
+   input wire clk, rst, ena;
+   input wire [N-1:0] ticks;
+   output logic       out;
 
-logic [N-1:0] counter;
-logic counter_comparator;
+   logic [N-1:0] counter;
+   logic [N-1:0] next_state;
 
+   // See schematic for details
+   always_comb begin
+      out = ~|counter;
+      next_state = ena ? ((counter == ticks) ? 'b0 : counter + 1)
+                       : counter;
+   end
 
+   always_ff @(posedge clk) begin
+      counter <= rst ? 'b0 : next_state;
+   end
 endmodule
