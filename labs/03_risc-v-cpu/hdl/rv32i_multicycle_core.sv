6fb452fdde1fb4e3a946b3936fbbe666838128b4
Devlin Ih <dih@olin.edu>
2022-11-27 20:20:26 -0500

    Add default case for immediate extension
    Good practice

--- labs/03_risc-v-cpu/hdl/rv32i_multicycle_core.sv
+++ labs/03_risc-v-cpu/hdl/rv32i_multicycle_core.sv
@@ -171,6 +171,7 @@ always_comb : begin : imm_ext
      IMM_B_TYPE : imm_ext = {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1’b0};
      IMM_J_TYPE : imm_ext = {{12{instr[31]}}, instr[19:12], instr[20], instr[30:21], 1’b0};
      IMM_U_TYPE : imm_ext = {{12{instr[31]}}, instr[31:12]};
+     default    : imm_ext = 32'b0;
    endcase
 end
 
